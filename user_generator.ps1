7486749bf0ec99963cc26c7d159ab948330ee9b4
ryanlangley4 <ryan.langley4@gmail.com>
2016-06-05 03:43:43 -0700

    6_4_2016
    added log path variable

--- user_generator.ps1
+++ user_generator.ps1
@@ -1,19 +1,24 @@
+While working on AD migrations and attempting to work on some pain points in AD auditing I decided that my home lab network looked a little lonely. Working with the API found on https://www.randomuser.me I built a script to 
+
 #
 #
 #preset variables for the script:
 #
  
 $date = Get-date -format M.d.yyyy
-$ou="OU=LANDING,DC=DEFAULTDOMAIN,DC=COM"
-$principlename = "@DEFAULT.com"
+$ou="OU=LANDING,DC=TEST,DC=LOCAL"
+$principlename = "@TEST.LOCAL"
 $description = "Test Account Generate $date"
 $Number_of_users = "5000"
-$company = "Test Company" 
+$company = "Test Company"
+
 #Supported Nationalities: AU, BR, CA, CH, DE, DK, ES, FI, FR, GB, IE, IR, NL, NZ, TR, US
 #Comma seperated values for multiple ie:
 #$nationalities ="US,DK,FR"
-$nationalities ="US"
- 
+$nationalities ="US,FR"
+
+#log path
+$log_path = ".\create_output.csv" 
 #
 #end of preset variables for script:
 #
@@ -50,13 +55,13 @@ function generate() {
     $letters_low=@("a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z")
     $letters_cap=@("A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z")
     $numbers=@("1","2","3","4","5","6","7","8","9","0")
-    $itterations = get-random -minimum 8 -maximum 10
+    $itterations = get-random -minimum 8 -maximum 17
     [string]$pass_value = ""
  
     for($i = 0; $i -ne $itterations+1;$i++) {
     $character_type = Get-random -minimum 1 -maximum 9
  
-    switch ($character_type) { 
+    switch ($character_type) {
         1 { $letter = Get-random -minimum 0 -maximum 26
         $pass_value = $pass_value+$character[$letter]
         }
@@ -98,7 +103,7 @@ return $pass_value
  
  
  
-echo "Status,Date,SamAccountName,Password,FirstName,LastName,DisplayName,City,Phone" >> create_output.csv
+echo "Status,Date,SamAccountName,Password,FirstName,LastName,DisplayName,City,Phone" >> $log_path
 $user_data_json_list = invoke-restmethod "https://www.randomuser.me/api/?results=$Number_of_users&nat=$nationalities" | select -expandproperty results
 foreach($user_data_json in $user_data_json_list) {
  
@@ -112,11 +117,11 @@ $aduser_samaccountname = find_ad_id $aduser_Given $aduser_Surname
    
     if($aduser_samaccountname -eq "ERROR:FAIL") {
         echo "Failure $aduser_Surname sam account in use"
-        echo "Error Samaccount,""$date"",""$aduser_samaccountname"",""$aduser_password"",""$aduser_Given"",""$aduser_Surname"",""$aduser_display"",""$aduser_city"",""$aduser_phone""" >> create_output.csv
+        echo "Error Samaccount,""$date"",""$aduser_samaccountname"",""$aduser_password"",""$aduser_Given"",""$aduser_Surname"",""$aduser_display"",""$aduser_city"",""$aduser_phone""" >> $log_path
     } else {
-        New-ADUser -AccountPassword (ConvertTo-SecureString â€œ$aduser_passwordâ€ -AsPlainText -Force) -ChangePasswordAtLogon $false -City $aduser_city -company â€œ$company_nameâ€ -DisplayName â€œ$aduser_displayâ€ -Enabled $true -MobilePhone â€œ$aduser_phoneâ€ -Name â€œ$aduser_displayâ€ -SamAccountName $aduser_samaccountname -Path â€œ$ou" -givenname $aduser_Given -surname $aduser_Surname -userprincipalname (â€œ$aduser_samaccountnameâ€ + â€œ$principlenameâ€) -description â€œ$descriptionâ€
+        New-ADUser -AccountPassword (ConvertTo-SecureString “$aduser_password” -AsPlainText -Force) -ChangePasswordAtLogon $false -City $aduser_city -company “$company_name” -DisplayName “$aduser_display” -Enabled $true -MobilePhone “$aduser_phone” -Name “$aduser_display” -SamAccountName $aduser_samaccountname -Path “$ou" -givenname $aduser_Given -surname $aduser_Surname -userprincipalname (“$aduser_samaccountname” + “$principlename”) -description “$description”
         clear
         Write-Host "`r Generating user: $aduser_Given $aduser_Surname" -NoNewLine
-        echo "Success,""$date"",""$aduser_samaccountname"",""$aduser_password"",""$aduser_Given"",""$aduser_Surname"",""$aduser_display"",""$aduser_city"",""$aduser_phone""" >> create_output.csv
+        echo "Success,""$date"",""$aduser_samaccountname"",""$aduser_password"",""$aduser_Given"",""$aduser_Surname"",""$aduser_display"",""$aduser_city"",""$aduser_phone""" >> $log_path
     }
 }
